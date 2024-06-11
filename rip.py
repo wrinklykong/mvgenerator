b7501bc1904f80bca53829ba3926796638db1900
wrinklykong <johnwjones1337@gmail.com>
2024-06-10 23:02:46 -0500

    Fix shell getting stuck

--- rip.py
+++ rip.py
@@ -38,11 +38,14 @@ def main():
         music_files = [f for f in os.listdir(full_dir_path) if f.endswith(".wav")]
         for song in music_files:
             # TODO: Use output_dir
-            generate_video(f"{args.root_dir}{directory}/{song}", full_dir_path, f"./{song}.mp4")
+            pass
+            # generate_video(f"{args.root_dir}{directory}/{song}", full_dir_path, f"./{song}.mp4")
+        generate_video(f"{args.root_dir}/{directory}/{music_files[2]}", full_dir_path, f"./{music_files[2]}.mp4")
+        print(music_files[2])
         # TODO: If all WAVs are successful, add it to the completed text
-        with open("completed.txt", "a") as f:
-            print("help")
-            f.write(directory + "\n")
+        # with open("completed.txt", "a") as f:
+        #     print("help")
+        #     f.write(directory + "\n")
 
     # TODO: Ignore the completed dirs in the txt file
     # TODO: Automate upload to YouTube? is that a thing lol
@@ -51,9 +54,10 @@ def generate_video(song_path, files_path, out_path):
     # cmd = ffmpeg -framerate 1/(length of track / number of the images) -i Front.png -i J-Majik\ -\ Shiatsu.wav -c:v libx264 -r 30 -pix_fmt yuv420p -shortest Shiatsu.mp4
     try:
         cmd = gen_cmd(song_path, files_path, out_path)
-        results = subprocess.run(cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(results.stderr)
-        print("cmd ran")
+        output = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, shell=True)
+        print(output.stdout)
+        print(cmd)
+        print(output.stderr)
     except Exception as e:
         print(e)
         pass
@@ -68,13 +72,14 @@ def gen_cmd(song_path, files_path, out_path):
         raise Exception(f"No images found in dir '{files_path}', quitting.")
     seconds_per_image = math.ceil(song_length / num_images)
     # Quotes being eaten for some reason
-    cmd = f"ffmpeg -framerate 1/{seconds_per_image} -pattern_type glob -i \"{images_glob}\" -i \"{song_path}\" -c:v libx264 -r 15 -tune stillimage -preset ultrafast -pix_fmt yuv420p -shortest \"{out_path}\""
+    cmd = f'ffmpeg -y -framerate 1/{seconds_per_image} -pattern_type glob -i \"{images_glob}\" -i \"{song_path}\" -c:v libx264 -r 15 -tune stillimage -preset ultrafast -pix_fmt yuv420p -shortest \"{out_path}\"'
     print(cmd)
     return cmd
 
 def get_length_of_audio_in_seconds(song_path):
     if not os.path.exists:
         raise Exception(f"File at path: '{song_path}' not found")
+    # This fails, change dir
     f = soundfile.SoundFile(song_path)
     seconds = f.frames / f.samplerate
     return seconds
